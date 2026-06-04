368e05e63c46184c3fdcb69ca332c15d6b34140b
yuyanghuang0718-sys <282432737+yuyanghuang0718-sys@users.noreply.github.com>
2026-06-04 21:10:30 +0800

    crop brand logo whitespace

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import chefPhoto from '../assets/chef-photo.png';
-import brandVisual from '../assets/timmy-dessert-logo-correct.png';
+import brandVisual from '../assets/timmy-dessert-logo-cropped.png';
 
 function Timeline({ content }) {
   return (
